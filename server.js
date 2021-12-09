6cbc587e39094b446e64a239b1d50d3b8209d0f2
Santiago García Cabral <stylder@gmail.com>
2021-12-09 15:32:30 -0600

    Feature: exceljs and dockerfile added

--- server.js
+++ server.js
@@ -1,28 +1,65 @@
-const express = require('express');
-const multer  = require('multer');
-const pdf2json = require('./pdf2json');
 const os = require('os');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors')
+const pdf2json = require('./pdf2json');
 const upload = multer({ dest: os.tmpdir() });
+const Excel = require('exceljs');
 
-
-// Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
 const app = express();
+const workbook = new Excel.Workbook();
 
-app.get('/', (req, res) => { res.send('ok') });
+app.use(cors())
 
-app.post('/upload', upload.single('file'), async (req, res) => {
-    if(!req?.file){
+app.get('/', (_, res) => { res.send('ok') });
+
+app.post('/pdf', upload.single('file'), async (req, res) => {
+    if (!req?.file) {
         res.sendStatus(500)
         return
     }
     const file = req.file;
-    const {path} = file
-    
-    pdf2json(path).then((r)=> {res.send(r)}).catch((e)=> res.sendStatus(500))
+    const { path } = file
+
+    pdf2json(path).then((r) => { res.send(r) }).catch((e) => res.sendStatus(500))
 });
 
-app.listen(PORT, HOST);
 
-console.log(`Running on http://${HOST}:${PORT}`);
+app.post('/xlsx', upload.single('file'), async (req, res) => {
+    if (!req?.file) {
+        res.sendStatus(500)
+        return
+    }
+    const file = req.file;
+    const { path } = file
+
+    workbook.xlsx.readFile(path).then(() => {
+        let worksheet = workbook.getWorksheet(1);
+
+        worksheet.spliceRows(0, 10);
+
+        let json = []
+
+        worksheet.eachRow( (row) => {
+            const { values } = row
+            const sim = {
+                ICCID: (values[1]),
+                IMEI: Number(values[2]),
+                ESTATUS: (values[3]),
+                RESPUESTA: (values[4]),
+                TELEFONO: Number(values[6])
+            }
+            
+            json.push(sim)
+        })
+
+        res.send(json)
+    })
+
+
+});
+
+app.listen(PORT);
+
+console.log(`Running on PORT: ${PORT}`);
